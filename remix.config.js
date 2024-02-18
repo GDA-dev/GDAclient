3d906a2237eae930f5e0a498a4b4b304851e1e42
Michael Girma <michaelgirma@wireless-10-104-38-10.umd.edu>
2024-02-18 16:48:26 -0500

    started deployment config

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  serverBuildTarget: "vercel",
   ignoredRouteFiles: ["**/.*"],
+  server: process.env.NODE_ENV === "development" ? undefined : "./build.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
